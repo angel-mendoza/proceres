512906f382724fcc872123af4c9223f6b1792d39
Angel Mendoza <mendozaangel003@gmail.com>
2016-07-07 16:04:59 -0430

    esta version es el inicio del proyecto, ya tengo el demo, personaje solo camina y salta pero las estacas te matan y la camara te sigue, ya esta listo el efecto de particulas cuando mueres y cuando renaces

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckPoint;
+
+    private PlayerController player;
+
+    public GameObject DeathParticle;
+    public GameObject LiveParticle;
+
+    public float respawnDelay;
+
+    // Use this for initialization
+    void Start () {
+        player = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RespawnPlayer(){
+        StartCoroutine("RespawnPlayerCo");
+    }
+
+    public IEnumerator RespawnPlayerCo()
+    {
+        Instantiate(DeathParticle, player.transform.position, player.transform.rotation);
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        Debug.Log("Player Respawn");
+        yield return new WaitForSeconds(respawnDelay);
+        player.transform.position = currentCheckPoint.transform.position;
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        Instantiate(LiveParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float grounsCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    private bool doubleJumped;
+
+    private Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate(){
+        grounded = Physics2D.OverlapCircle(groundCheck.position, grounsCheckRadius, whatIsGround);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+//-----codigo para que salte----------------------------
+
+        if (grounded)
+        {
+            doubleJumped = false;
+        }
+
+        anim.SetBool("Ground", grounded);
+            
+        if (Input.GetKeyDown (KeyCode.Space) && grounded)
+        {
+            jump();
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !doubleJumped && !grounded)
+        {
+            jump();
+            //GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            doubleJumped = true; 
+        }
+
+//------codigo para que camine----------------------------
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+        if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            transform.localScale = new Vector3(7f, 7f, 1f);
+        else if (GetComponent<Rigidbody2D>().velocity.x < 0)
+            transform.localScale = new Vector3(-7f, 7f, 1f);
+    }
+
+    public void jump()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+    }
+}
