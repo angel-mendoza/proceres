1811b425f7ca7107dff7c7fe3ef848833378936e
Angel Mendoza <mendozaangel003@gmail.com>
2016-08-06 02:18:20 -0430

    ya tiene el menu de seleccion de personaje, tiene musica y la imagen de loading con su barra de progreso, los botones de menu estan funcionales y te redireccionan a donde deben

---
+++ Scripts/ChangeMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+    public AudioClip leve2Music;
+    private AudioSource sourse;
+
+	// Use this for initialization
+	void Awake () {
+        sourse = GetComponent<AudioSource>();
+	}
+	
+    void OnLevelWasLoaded(int level)
+    {
+        if (level != 0)
+        {
+            sourse.clip = leve2Music;
+            sourse.Play();
+        }
+    }
+}
