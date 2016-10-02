06001a3c4b971ad0c0ff661788252abdd3be5baa
Angel Mendoza <mendozaangel003@gmail.com>
2016-10-01 19:47:47 -0430

    ataque combo bola de fuego hecho y ya tiene ki!

---
+++ Scripts/ComboAtackManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComboAtackManager : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, -GetComponent<Rigidbody2D>().velocity.y);
+
+	}
+}
--- Scripts/EnergyBarManager.cs
+++ Scripts/EnergyBarManager.cs
@@ -6,18 +6,28 @@ public class EnergyBarManager : MonoBehaviour {
 
     public Slider energybar;
     public float tiempo;
+    public float tiempoEspera;
+    public bool dispararCombo;
+    
 
 	// Use this for initialization
 	void Start () {
 
         energybar = GetComponent<Slider>();
+        dispararCombo = false;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        energybar.maxValue = tiempoEspera;
         tiempo += Time.deltaTime;
         energybar.value = tiempo;
+
+        if (tiempo >= tiempoEspera)
+        {
+            dispararCombo = true;
+        }
+        
 	}
 }
