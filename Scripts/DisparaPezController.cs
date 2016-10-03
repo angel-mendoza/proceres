8e48c2fb6154e64db22e377e9b1f5341016b369d
Angel Mendoza <mendozaangel003@gmail.com>
2016-10-02 23:49:40 -0400

    casi listos los enemigos! tenemos a los españoles, moscas, abejas, peces jajajajajajaja lo tipico en un plataforma game

---
+++ Scripts/DisparaPezController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisparaPezController : MonoBehaviour {
+    public Transform firePoints;
+    public GameObject pez;
+    GameObject pezclon;
+    public float tiempo;
+    public float tiempoDisparo;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tiempo += Time.deltaTime;
+        if (tiempo >= tiempoDisparo)
+        {
+            pezclon = Instantiate(pez, firePoints.transform.position, Quaternion.identity) as GameObject;
+            tiempo = 0;
+        }
+       
+	}
+}
---
+++ Scripts/PezController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PezController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public int daño;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, speed);
+    }
+
+}
